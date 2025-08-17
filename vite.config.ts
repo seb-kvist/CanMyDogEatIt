c7d008f830614a1e413a86b23f89d0796e920d55
Sebastian Kvist <sebastian_kvist@hotmail.com>
2025-08-17 23:34:57 +0200

    Update main with latest changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/CanMyDogEatIt/" : undefined,
   server: {
-    host: "::",
+    host: "localhost",
     port: 8080,
   },
   plugins: [
